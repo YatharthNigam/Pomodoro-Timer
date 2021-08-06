c82cce87555fc90fb1af2b8e0586879dfa9a8035
Yatharth Nigam <yatharthnigam@gmail.com>
2021-08-06 10:03:11 +0530

    day 2 commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import './App.css';
-import Timer from './Timer.js';
-import Settings from './Settings.js';
-import {useState} from "react";
-
+import "./App.css";
+import Timer from "./Timer.js";
+import Settings from "./Settings.js";
+import { useState } from "react";
 
 function App() {
+  const [showSettings, setShowSettings] = useState(true);
 
-  const [showSettings, setShowSettings] = useState(initialState: false);
-
-  return (
-    <main>
-      {showSettings ? <Settings/> : <Timer />}
-    </main>
-  );
+  return <main>
+  {showSettings ? <Settings /> : <Timer />}</main>;
 }
 
 export default App;
--- src/Settings.js
+++ src/Settings.js
@@ -1,10 +1,29 @@
-function Settings()
-{
-    return(
-        <div>
-            settings
-        </div>
-    )
+import ReactSlider from "react-slider";
+import "./slider.css";
+
+function Settings() {
+  return (
+    <div style={{ textAlign: "left" }}>
+      <label>Work Minutes</label>
+      <ReactSlider
+        className={"slider"}
+        thumbClassName={"thumb"}
+        trackClassName={"track"}
+        value={45}
+        min={1}
+        max={120}
+      ></ReactSlider>
+      <label>Break Minutes</label>
+      <ReactSlider
+        className={"slider break"}
+        thumbClassName={"thumb"}
+        trackClassName={"track"}
+        value={45}
+        min={1}
+        max={120}
+      ></ReactSlider>
+    </div>
+  );
 }
 
 export default Settings;
